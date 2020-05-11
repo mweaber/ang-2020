cb9931dd72c29d504fd93c945c1dcaa64d90583a
Matthew Weaber <matthewweaber89@gmail.com>
2020-05-11 11:46:16 -0400

    Adding outlines and data

--- recipe-book/src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'Desc Ex Text', 'https://media.newyorker.com/photos/5d13d9c28be7a80008baaf21/master/w_1280%2Cc_limit/Zeller-Cream-Cheese.jpg')
+  ];
 
   constructor() { }
 
